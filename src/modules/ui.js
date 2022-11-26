522dc5a9df80e3d7de5d9d81faca515ef9d21253
Tolga Gonul <tolgon@gmail.com>
2022-11-26 15:00:15 +0100

    Divided task view into snippet / detailed view that will expand by clicking the chevron.

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,14 +1,25 @@
-import '@fortawesome/fontawesome-free/js/fontawesome'
+import fontawesome from '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
+fontawesome.config = { autoReplaceSvg: false };
+
 const navButtons = document.querySelectorAll(".nav-button");
+const chevronDown = document.querySelectorAll(".fa-chevron-down");
 
 navButtons.forEach(button => {
   button.addEventListener('click', (e) => {
     if(e.target.classList.contains("active")) return;
     setActiveButton(button);
+  })
+})
+
+chevronDown.forEach(button => {
+  console.log(button);
+  button.addEventListener('click', (e) => {
+    console.log(e);
+    console.log("yo");
   })
 })
 
