27b228420d3c74398059858ccd83aa92033c6bc7
Tolga Gonul <tolgon@gmail.com>
2022-11-27 21:44:46 +0100

    Added expand/collapse to task view

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,34 +1,53 @@
-import fontawesome from '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
 
-fontawesome.config = { autoReplaceSvg: false };
+(function initiateButtonListeners() {
+  const navButtons = document.querySelectorAll(".nav-button");
+  const taskExpandButtons = document.querySelectorAll(".fa-expand");
+  const taskCollapseButtons = document.querySelectorAll(".fa-collapse");
+  const taskDetailedView = document.querySelectorAll(".task-details");
 
-const navButtons = document.querySelectorAll(".nav-button");
-const chevronDown = document.querySelectorAll(".fa-chevron-down");
-
-navButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    if(e.target.classList.contains("active")) return;
-    setActiveButton(button);
-  })
-})
-
-chevronDown.forEach(button => {
-  console.log(button);
-  button.addEventListener('click', (e) => {
-    console.log(e);
-    console.log("yo");
-  })
-})
-
-function setActiveButton(button) {
   navButtons.forEach(button => {
-    if (button !== this) {
-      button.classList.remove("active");
-    }
-  });
+    button.addEventListener("click", (e) => {
+      if(e.target.classList.contains("active")) return;
+      setActiveButton(button);
+    })
+  })
 
-  button.classList.add("active");
-}
+  taskExpandButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+      let index = e.target.parentNode.parentNode.parentNode.parentNode.dataset.index;
+      expandDetails(index);
+    })
+  })
+
+  taskCollapseButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+      let index = e.target.parentNode.parentNode.parentNode.parentNode.dataset.index;
+      collapseDetails(index);
+    })
+  })
+
+  function expandDetails(index) {
+    taskExpandButtons[index].classList.add("hide");
+    taskCollapseButtons[index].classList.remove("hide");
+    taskDetailedView[index].classList.remove("hide");
+  }
+
+  function collapseDetails(index) {
+    taskCollapseButtons[index].classList.add("hide");
+    taskExpandButtons[index].classList.remove("hide");
+    taskDetailedView[index].classList.add("hide");
+  }
+
+  function setActiveButton(button) {
+    navButtons.forEach(button => {
+      if (button !== this) {
+        button.classList.remove("active");
+      }
+    })
+    button.classList.add("active");
+  }
+})();
+
